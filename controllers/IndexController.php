09bbb99a1a18230c86844e9c39e113f8de35baa6
Arijit Panigrahi <arijitpanigrahi10@gmail.com>
2019-08-19 13:49:08 +0530

    block controller added

---
+++ controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+class Codilar_ShippingRates_IndexController extends Mage_Core_Controller_Front_Action
+{
+     public function indexAction()
+     {
+         
+     	//echo 'This is a test';
+     	$this->loadLayout();
+     	$this->renderLayout();
+
+     }
+
+      
+}
