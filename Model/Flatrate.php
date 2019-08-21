1d8025a2d0822cd0cbb67dc1a99afcd063d75f1f
Sanjukta Chowdhury <donalovesanime@gmail.com>
2019-08-21 13:01:57 +0530

    update

--- Model/Flatrate.php
+++ Model/Flatrate.php
@@ -5,41 +5,53 @@ class Codilar_ShippingRates_Model_Flatrate
    {
 
    protected $_code = 'flatrate';
-
+   
 
     public function collectRates(Mage_Shipping_Model_Rate_Request $request)
     {
-    if (!$this->getConfigFlag('active')) {
-        return false;
-    }
+        $var1= Mage::getSingleton('core/session')->getPincode();
+        // echo $var1;
+        // die;
+        if (!$this->getConfigFlag('active')) 
+        {
+            return false;
+        }
 
-    $freeBoxes = 0;
-    if ($request->getAllItems()) {
-        foreach ($request->getAllItems() as $item) {
-            if ($item->getFreeShipping() && !$item->getProduct()->getTypeInstance()->isVirtual()) {
-                $freeBoxes+=$item->getQty();
+        $freeBoxes = 0;
+        $quote = Mage::getSingleton('checkout/session')->getQuote();
+        if ($quote->getAllVisibleItems()) 
+        {
+
+            foreach ($quote->getAllVisibleItems() as $item) 
+            {
+                if ($item->getFreeShipping() && !$item->getProduct()->getTypeInstance()->isVirtual()) 
+                {
+                    $freeBoxes+=$item->getQty();
+                }
             }
         }
-    }
-    $this->setFreeBoxes($freeBoxes);
-
-    $result = Mage::getModel('shipping/rate_result');
-    if ($this->getConfigData('type') == 'O') { // per order
-        $shippingPrice = $this->getConfigData('price');
-    } elseif ($this->getConfigData('type') == 'I') { // per item
-        $shippingPrice = '0.00';
-        foreach ($request->getAllItems() as $item) {
-            if (!$item->getFreeShipping()) {
-                //$productId = 700091;//$item->getProduct()->getId();
-                $productList = Mage::getModel('shippingrates/shippingzonerates')->getCollection();
-                   $productdata = $productList->addFieldToSelect('*')->addFieldToFilter('postcode',700091)->getData();
-                   //print_r ($productdata);
-                   //die;
-                foreach($productdata as $product) {
+        $this->setFreeBoxes($freeBoxes);
+        $result = Mage::getModel('shipping/rate_result');
+        if ($this->getConfigData('type') == 'O') 
+        { // per order
+            $shippingPrice = $this->getConfigData('price');
+        } 
+        elseif ($this->getConfigData('type') == 'I') 
+        {  //per item
+            $shippingPrice = '0.00';
+            foreach ($quote->getAllVisibleItems() as $item) 
+            {
+                if (!$item->getFreeShipping()) 
+                {
+                  $productList = Mage::getModel('shippingrates/shippingzonerates')->getCollection();
+                  $productdata = $productList->addFieldToSelect('*')->addFieldToFilter('postcode',$var1)->getData();
+                 foreach($productdata as $product) 
+                 {
                     $quantity = $item->getQty();
                     // if ($product->getRate()) {
-                    $product1 = $product['rate'];
-                        $shippingPrice+=$product1 * $quantity;
+                    $product1 = $product['rate']; 
+                    $shippingPrice+=$product1 * $quantity;
+
                    //  } else {
                     //     $shippingPrice+=$this->getConfigData('price') * $quantity;
 
@@ -48,7 +60,8 @@ class Codilar_ShippingRates_Model_Flatrate
             }
 
         }
-    } else {
+    }
+     else {
         $shippingPrice = false;
     }
 
--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -12,10 +12,14 @@ class Codilar_ShippingRates_IndexController extends Mage_Core_Controller_Front_A
      }
  public function getpriceAction()
      {
-         Mage::log('My model shipping rates entry', null, 'ajax.log'); // //Static text
-     	return 1;
-           
+         Mage::log('My model shipping rates entry', null, 'ajax.log'); 
+         $pin = $this->getRequest()->getParam('pincode');
+         Mage::getSingleton('core/session')->setPincode($pin);
+//          $product = Mage::registry('current_product');
+// $id = $product->getId();
+// Mage::getSingleton('core/session')->setProduct_id_type($id);
      }
 
       
+
 }
